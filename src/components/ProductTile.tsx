257170adc228513826776c7128bce5620af19b4c
Dominik Maćkiewicz <dominik.mackiewicz@nexio.pl>
2024-03-25 12:23:11 +0100

    remove unused imports

--- src/components/ProductTile.tsx
+++ src/components/ProductTile.tsx
@@ -1,5 +1,5 @@
-import { Box, HStack, Spacer, Text } from "@chakra-ui/react";
-import { ColorValueHex, Product } from "../models/product";
+import { Box, Text } from "@chakra-ui/react";
+import { Product } from "../models/product";
 import ProductModal from "./Modal";
 import { useState } from "react";
 
